b94a5f6316e69afd4ebe8c48bd3ad0a62f350d9f
woffordlm <lukewofford@gmail.com>
2022-04-17 21:37:58 -0400

    added forgotten imports

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { useMutation } from "@apollo/react-hooks"
 import { createUser } from '../utils/API';
+import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from "@apollo/react-hooks";
+import { SAVE_BOOK } from "../utils/mutations"
 
 const SearchBooks = () => {
   // create state for holding returned google api data
